82747486ccc615be9fd1c792412e45c6854d4f5e
Khom <mgrprinc37@gmail.com>
2022-01-20 15:46:58 +0545

    ikhom: imported handTrackingModule that we created last time

--- src code/FingerCounter/fingerCounter.py
+++ src code/FingerCounter/fingerCounter.py
@@ -5,8 +5,8 @@
 
 import os
 import cv2
-import mediapipe as mp
 import time
+import HandTrackingModule as htm
 
 wCam, hCam = 640, 480
 
@@ -26,17 +26,32 @@ for imPath in myList:
     overlayList.append(image)
 
 print(len(overlayList))
+pTime = 0
+
+detector = htm.handDetector(detectionCon=0.75)
+
 
 while True:
 
     success, img = cap.read()
+    img = detector.findHands(img)
 
-    # img[0:200, 0:200] = overlayList[0]
-    img[0:228, 0:221] = overlayList[0]  # slicing img[0:200,0:200] height and width
+    h, w, c = overlayList[0].shape
+    img[0:h, 0:w] = overlayList[0]
+    #
+    # # img[0:200, 0:200] = overlayList[0]
+    # img[0:228, 0:221] = overlayList[0]  # slicing img[0:200,0:200] height and width
+    # # img[100:328, 100:321] = overlayList[0]
 
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 0), 3)  # font, scale, color, thickness
     cv2.imshow("Finger Counter", img)
     # cv2.waitKey(1)
 
     if cv2.waitKey(1) == 13:
         break
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
